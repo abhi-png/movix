be84ea1363696058a072aef57516062707b8f667
Abinash Kar <abinash.kar611@gmail.com>
2023-08-28 11:33:48 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "https://movix.abinashkar.com/",
-  plugins: [react()]
+  plugins: [react()],
+  base: "https://movix.abinashkar.com/"
 })
